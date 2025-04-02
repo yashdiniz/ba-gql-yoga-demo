5c26b01e0ca136544f8a45c11950e971df9fc3a1
yashdiniz <26359876+yashdiniz@users.noreply.github.com>
2025-04-02 17:21:43 +0530

    chore: use pglite driver

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   out: './drizzle',
   schema: './db/schema.ts',
   dialect: 'postgresql',
+  driver: 'pglite',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
