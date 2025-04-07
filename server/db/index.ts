72643109098acbec1bf0514ef966ddb845f82972
yashdiniz <26359876+yashdiniz@users.noreply.github.com>
2025-04-07 17:14:57 +0530

    fix: add schema to database config.

--- server/db/index.ts
+++ server/db/index.ts
@@ -2,8 +2,10 @@ import 'dotenv/config'
 
 import { PGlite } from '@electric-sql/pglite'
 import { drizzle } from 'drizzle-orm/pglite'
+import * as schema from './schema'
 
 const client = new PGlite(process.env.DATABASE_URL!)
 export const db = drizzle({
     client,
+    schema,
 })
